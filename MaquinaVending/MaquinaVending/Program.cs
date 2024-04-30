b7530c5f632a4165d4771b4c856e2faa6dcc92dc
Gonzalo Zabala Salinas <9101670@alumnos.ufv.es>
2024-04-30 16:01:57 +0200

    Revisado y va perfe

--- MaquinaVending/MaquinaVending/Program.cs
+++ MaquinaVending/MaquinaVending/Program.cs
@@ -9,14 +9,6 @@ namespace MaquinaVending
 {
     internal class Program
     {
-        //Errores
-        //No va el Precio Total
-        //No va el ver si el carrito esta vacío 
-        //Errores con el parse
-
-        //Falta modificar cantidades y poner que no se pueda comprar si la lista esta vacia
-
-
         static List <Producto> listaProductos;
 
         static Usuario usuario;
